29bac7f0dfaf61bd5b7197ac18f5d690eed9d133
Marc-André Marceau <marc-andre.marceau@nuvoola.com>
2019-09-25 09:48:00 -0400

    options

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ var close = exports.close = () => {
 }
 //process.on('exit', close)
 
-exports.init = async function (imgsrc) {
-  model = TFModel({
+var options
+
+exports.init = async function (opt = {}) {
+
+  options = Object.assign({
     'gpu_memory_fraction': 0.10,
+    'minimum_score': 0.3,
+  }, opt)
+
+  model = TFModel({
+    'gpu_memory_fraction': options.gpu_memory_fraction,
   })  
   //await model.load(`${__dirname}/models/ssdlite_mobilenet_v2_coco_2018_05_09.pb`)
   await model.load(`${__dirname}/models/ssd_inception_v2_coco_2018_01_28.pb`)
@@ -27,22 +35,22 @@ exports.init = async function (imgsrc) {
   }  
 }
 
-async function pad_image(img, padheight, padwidth) {
-  var height = img.rows
-  var width = img.cols
-  var channel = img.channels
+// async function pad_image(img, padheight, padwidth) {
+//   var height = img.rows
+//   var width = img.cols
+//   var channel = img.channels
 
-  if(height > padheight || width > padwidth) {
-    width = (width > padwidth) ? padwidth : width
-    height = (height > padheight) ? padheight : height
-    img = await img.getRegion(new cv.Rect(0, 0, width, height)).copyAsync()
-  }
-  if(height < padheight || width < padwidth) {
-    var img = await img.copyMakeBorderAsync(0, padheight - height, 0, padwidth - width, cv.BORDER_CONSTANT, new cv.Vec3(0, 0, 0))
-  }
+//   if(height > padheight || width > padwidth) {
+//     width = (width > padwidth) ? padwidth : width
+//     height = (height > padheight) ? padheight : height
+//     img = await img.getRegion(new cv.Rect(0, 0, width, height)).copyAsync()
+//   }
+//   if(height < padheight || width < padwidth) {
+//     var img = await img.copyMakeBorderAsync(0, padheight - height, 0, padwidth - width, cv.BORDER_CONSTANT, new cv.Vec3(0, 0, 0))
+//   }
  
-  return img
-}
+//   return img
+// }
 
 async function imgResize(img, width, height) {
   var sharpresize = await sharp(img.getData(), {
@@ -77,7 +85,7 @@ exports.detect = async function (img) {
   var num_detections = val.num_detections.data[0]
   for (var i = 0; i < num_detections; i++) {
     var score = val.detection_scores.data[i]
-    if(score > 0.3) {
+    if(score > options.minimum_score) {
       var detection_boxes = val.detection_boxes.data.subarray(4*i,4*(i+1))
       var [ymin, xmin, ymax, xmax] = detection_boxes
       var label = labels[val.detection_classes.data[i]]
